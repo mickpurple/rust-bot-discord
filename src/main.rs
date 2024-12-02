53e7304eff7d67b401e9bd4f0db15c4f03c5b1ca
mickpurple <themickpurple@gmail.com>
2024-12-02 13:27:20 +0530

    updated with slash commands

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,71 @@
 use dotenvy::dotenv;
-use serenity::async_trait;
-use serenity::model::channel::Message;
-use serenity::prelude::*;
+// use serenity::async_trait;
+// use serenity::model::channel::Message;
+// use serenity::prelude::*;
+
+use poise::serenity_prelude as serenity;
+use reqwest;
+use serde_json;
 use std::env;
-use std::fs;
-use std::io::{prelude::*, BufReader};
-// use std::net::{TcpListener, TcpStream};
 use tokio::sync::oneshot;
 use warp::Filter;
 
-struct Handler;
+// struct Handler;
 
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        async fn reply(ctx: Context, msg: Message, text: String) {
-            if let Err(why) = msg.channel_id.say(&ctx.http, text).await {
-                println!("Error sending message: {why:?}");
-            }
-        }
+// #[async_trait]
+// impl EventHandler for Handler {
+//     async fn message(&self, ctx: Context, msg: Message) {
+//         async fn reply(ctx: Context, msg: Message, text: String) {
+//             if let Err(why) = msg.channel_id.say(&ctx.http, text).await {
+//                 println!("Error sending message: {why:?}");
+//             }
+//         }
 
-        match msg.content.as_str() {
-            "!ping" | "!pung" => reply(ctx, msg, String::from("Womp Womp!")).await,
-            "!help" => reply(ctx, msg, String::from("Heya! I am simplistic moderating discord bot, I was created by mickpurple using RUST and Serenity!!")).await,
-            _ => reply(ctx, msg, String::from("")).await,
-        };
-    }
+//         match msg.content.as_str() {
+//             "!ping" | "!pung" => reply(ctx, msg, String::from("Womp Womp!")).await,
+//             "!help" => reply(ctx, msg, String::from("Heya! I am simplistic moderating discord bot, I was created by mickpurple using RUST and Serenity!!")).await,
+//             _ => reply(ctx, msg, String::from("")).await,
+//         };
+//     }
+// }
+
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+/// Displays your or another user's account creation date
+#[poise::command(slash_command, prefix_command)]
+async fn age(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    ctx.say(response).await?;
+    Ok(())
 }
 
-// fn handle_connection(mut stream: TcpStream) {
-//     let buf_reader = BufReader::new(&mut stream);
-//     let request_line = buf_reader.lines().next().unwrap().unwrap();
+/// Shows a random meme from Reddit
+#[poise::command(slash_command, prefix_command)]
+async fn meme(ctx: Context<'_>) -> Result<(), Error> {
+    let mut m: String = String::from("meme");
 
-//     let (status_line, filename) = match &request_line[..] {
-//         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-//         _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
-//     };
+    let response = reqwest::get("https://meme-api.com/gimme/meme").await?;
 
-//     let contents = fs::read_to_string(filename).unwrap();
-//     let length = contents.len();
+    // Check the status code
+    if response.status().is_success() {
+        // Parse the response body (e.g., as JSON)
+        let data: serde_json::Value = response.json().await?;
+        println!("Data: {:?}", data);
+        m = data["preview"][3].as_str().unwrap().to_string();
+    } else {
+        println!("Error: {}", response.status());
+    }
 
-//     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-//     stream.write_all(response.as_bytes()).unwrap();
-// }
+    let response = format!("{}", m);
+    ctx.say(response).await?;
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
@@ -52,20 +74,26 @@ async fn main() {
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+    let intents = serenity::GatewayIntents::non_privileged();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![age(), meme()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
 
     // Create a new instance of the Client, logging in as a bot.
-    let mut client = Client::builder(&token, intents)
-        .event_handler(Handler)
+    let mut client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
         .await
         .expect("Err creating client");
-
-    // Start listening for events by starting a single shard
-    // if let Err(why) = client.start().await {
-    //     println!("Client error: {why:?}");
-    // }
 
     // Spawn the bot in a separate task
     let (tx, rx) = oneshot::channel();
@@ -76,17 +104,7 @@ async fn main() {
         tx.send(()).unwrap();
     });
 
-    // start web server
-    // let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-
-    // for stream in listener.incoming() {
-    //     let stream = stream.unwrap();
-
-    //     handle_connection(stream);
-    // }
-
     let route = warp::path::end().map(|| warp::reply::html("Hello, world!"));
-
     let server = warp::serve(route).bind(([0, 0, 0, 0], 8080));
 
     // Run the web server and bot concurrently
